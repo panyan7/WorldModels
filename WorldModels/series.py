d81918b427542652e45ac8bea8978d76d41677df
panyan7 <topanyan@sina.com>
2021-02-06 21:43:39 +0800

    Reformat code

--- WorldModels/series.py
+++ WorldModels/series.py
@@ -18,11 +18,12 @@ model_path_name = "results/{}/{}/tf_vae".format(args.exp_name, args.env_name)
 if not os.path.exists(SERIES_DIR):
     os.makedirs(SERIES_DIR)
 
+
 def ds_gen():
-    filenames = os.listdir(DATA_DIR)[:10000] # only use first 10k episodes
+    filenames = os.listdir(DATA_DIR)[:10000]  # only use first 10k episodes
     n = len(filenames)
     for j, fname in enumerate(filenames):
-        if not fname.endswith('npz'): 
+        if not fname.endswith('npz'):
             continue
         file_path = os.path.join(DATA_DIR, fname)
 
@@ -32,8 +33,8 @@ def ds_gen():
         reward = data['reward']
         done = data['done']
         N = data['N']
-        
-        n_pad = args.max_frames - img.shape[0] # pad so they are all a thousand step long episodes
+
+        n_pad = args.max_frames - img.shape[0]  # pad so they are all a thousand step long episodes
         img = tf.pad(img, [[0, n_pad], [0, 0], [0, 0], [0, 0]])
         action = tf.pad(action, [[0, n_pad], [0, 0]])
         reward = tf.pad(reward, [[0, n_pad]])
@@ -41,22 +42,30 @@ def ds_gen():
         N = tf.pad(N, [[0, n_pad]], constant_values=N[-1])
         yield img, action, reward, done, N
 
+
 def create_tf_dataset():
-    dataset = tf.data.Dataset.from_generator(ds_gen, output_types=(tf.float32, tf.float32, tf.float32, tf.bool, tf.uint16), output_shapes=((args.max_frames, 64, 64, 3), (args.max_frames, args.a_width), (args.max_frames,), (args.max_frames,), (args.max_frames,)))
+    dataset = tf.data.Dataset.from_generator(ds_gen,
+                                             output_types=(tf.float32, tf.float32, tf.float32, tf.bool, tf.uint16),
+                                             output_shapes=(
+                                             (args.max_frames, 64, 64, 3), (args.max_frames, args.a_width),
+                                             (args.max_frames,), (args.max_frames,), (args.max_frames,)))
     return dataset
+
 
 @tf.function
 def encode_batch(batch_img):
-  simple_obs = batch_img/255.0
-  mu, logvar = vae.encode_mu_logvar(simple_obs)
-  return mu, logvar
+    simple_obs = batch_img / 255.0
+    mu, logvar = vae.encode_mu_logvar(simple_obs)
+    return mu, logvar
+
 
 def decode_batch(batch_z):
-  # decode the latent vector
-  batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
-  batch_img = np.round(batch_img).astype(np.uint8)
-  batch_img = batch_img.reshape(batch_size, 64, 64, 3)
-  return batch_img
+    # decode the latent vector
+    batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
+    batch_img = np.round(batch_img).astype(np.uint8)
+    batch_img = batch_img.reshape(batch_size, 64, 64, 3)
+    return batch_img
+
 
 filelist = os.listdir(DATA_DIR)
 filelist.sort()
@@ -74,27 +83,27 @@ r_dataset = []
 d_dataset = []
 N_dataset = []
 
-i=0
+i = 0
 for batch in dataset:
-  i += 1
-  obs_batch, action_batch, r, d, N = batch
-  obs_batch = tf.squeeze(obs_batch, axis=0)
-  action_batch = tf.squeeze(action_batch, axis=0)
-  r = tf.reshape(r, [-1, 1])
-  d = tf.reshape(d, [-1, 1])
-  N = tf.reshape(N, [-1, 1])
+    i += 1
+    obs_batch, action_batch, r, d, N = batch
+    obs_batch = tf.squeeze(obs_batch, axis=0)
+    action_batch = tf.squeeze(action_batch, axis=0)
+    r = tf.reshape(r, [-1, 1])
+    d = tf.reshape(d, [-1, 1])
+    N = tf.reshape(N, [-1, 1])
 
-  mu, logvar = encode_batch(obs_batch)
+    mu, logvar = encode_batch(obs_batch)
 
-  mu_dataset.append(mu.numpy().astype(np.float16))
-  logvar_dataset.append(logvar.numpy().astype(np.float16))
-  action_dataset.append(action_batch.numpy())
-  r_dataset.append(r.numpy().astype(np.float16))
-  d_dataset.append(d.numpy().astype(np.bool))
-  N_dataset.append(N.numpy().astype(np.uint16))
+    mu_dataset.append(mu.numpy().astype(np.float16))
+    logvar_dataset.append(logvar.numpy().astype(np.float16))
+    action_dataset.append(action_batch.numpy())
+    r_dataset.append(r.numpy().astype(np.float16))
+    d_dataset.append(d.numpy().astype(np.bool))
+    N_dataset.append(N.numpy().astype(np.uint16))
 
-  if ((i+1) % 100 == 0):
-    print(i+1)
+    if ((i + 1) % 100 == 0):
+        print(i + 1)
 
 action_dataset = np.array(action_dataset)
 mu_dataset = np.array(mu_dataset)
@@ -103,4 +112,5 @@ r_dataset = np.array(r_dataset)
 d_dataset = np.array(d_dataset)
 N_dataset = np.array(N_dataset)
 
-np.savez_compressed(os.path.join(SERIES_DIR, "series.npz"), action=action_dataset, mu=mu_dataset, logvar=logvar_dataset, reward=r_dataset, done=d_dataset, N=N_dataset)
+np.savez_compressed(os.path.join(SERIES_DIR, "series.npz"), action=action_dataset, mu=mu_dataset, logvar=logvar_dataset,
+                    reward=r_dataset, done=d_dataset, N=N_dataset)
